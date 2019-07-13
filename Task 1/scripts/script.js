c7e89a2e2db7aa3e1f7a2a6eb5a88a30f7c58882
YuraTovstyzhenko <yuratov1998@gmail.com>
2019-07-13 20:27:34 +0300

    Task 1 Done

--- Task 1/scripts/script.js
+++ Task 1/scripts/script.js
@@ -1,4 +1,29 @@
   'use strict';
 
-let ask = prompt("Hello", "");
-console.log(typeof(ask));
+  let money = prompt("Ваш бюджет на месяц ?");
+  let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+  let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце");
+  let secondQuestion = prompt("Во сколько обойдется?");
+  let thirdQuestion = prompt("Введите обязательную статью расходов в этом месяце");
+  let fourthQuestion = prompt("Во сколько обойдется?");
+
+  let appData = {
+    budjet: money,
+    timeData: time,
+    expenses: {
+     // firstQuestion: secondQuestion,
+     // thidQuestion: fourthQuestion,
+    },
+    optionalExpenses: {
+
+    },
+    income: [
+
+    ],
+    savings: false,
+  };
+  appData.expenses.firstQuestion = secondQuestion;
+  appData.expenses.thirdQuestion = fourthQuestion;
+
+  alert(appData.budjet/30);
