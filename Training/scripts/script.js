9b3735aed7e85089eaae5529cacd8ae3a1c658ec
YuraTovstyzhenko <yuratov1998@gmail.com>
2019-07-16 15:52:23 +0300

    add Training directory

---
+++ Training/scripts/script.js
@@ -0,0 +1,19 @@
+let num = 50;
+
+switch (num) {
+    case num < 40:
+        console.log("1");
+        //break;
+    case num > 60:
+        console.log("2");
+      //  break;
+    case 50:
+        console.log('3');
+      //  break; 
+    case num <= 50:
+        console.log('33');
+      //  break; 
+    default:
+        console.log("000");
+      //  break;
+}
