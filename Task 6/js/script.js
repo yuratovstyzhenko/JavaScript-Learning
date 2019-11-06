7fb29f917e4c0a4bfa8fbec4c26b89a34bf7d111
YuraTovstyzhenko <yuratov1998@gmail.com>
2019-11-07 00:16:40 +0200

    Small fixes

--- Task 6/js/script.js
+++ Task 6/js/script.js
@@ -1,20 +1,19 @@
 let startCounting = document.getElementById('start');
 
-let budgetValue = document.getElementsByClassName('budget-value');
-let daybudgetValue = document.getElementsByClassName('daybudget-value');
-let levelValue = document.getElementsByClassName('level-value');
-let expensesValue = document.getElementsByClassName('expenses-value');
-let optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value');
-let incomeValue = document.getElementsByClassName('income-value');
-let monthsavingsValue = document.getElementsByClassName('monthsavings-value');
-let yearsavingsValue = document.getElementsByClassName('yearsavings-value');
+let budgetValue = document.getElementsByClassName('budget-value')[0];
+let daybudgetValue = document.getElementsByClassName('daybudget-value')[0];
+let levelValue = document.getElementsByClassName('level-value')[0];
+let expensesValue = document.getElementsByClassName('expenses-value')[0];
+let optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0];
+let incomeValue = document.getElementsByClassName('income-value')[0];
+let monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0];
+let yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0];
 
 let expensesItem = document.getElementsByClassName('expenses-item');
 
-let expensesButtons = document.getElementsByTagName('button');
-let firstButton = expensesButtons[0];
-let secondButton = expensesButtons[1];
-let thirdButton = expensesButtons[2];
+let firstExpensesButtons = document.getElementsByTagName('button')[0];
+let secondExpensesButtons = document.getElementsByTagName('button')[1];
+let thirdExpensesButtons = document.getElementsByTagName('button')[2];
 
 let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
 
